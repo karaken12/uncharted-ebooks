396b2d3ff25d695f9824e244fab7c9fc92267d9e
Tom Potts <karaken12@gmail.com>
2015-06-24 11:40:17 +0100

    Adding bare chapter layout for including in ebook.

--- build.sh
+++ build.sh
@@ -1,1 +1,4 @@
 jekyll build
+# See https://puppetlabs.com/blog/automated-ebook-generation-convert-markdown-epub-mobi-pandoc-kindlegen for more info
+mkdir -p _site/files
+pandoc -o _site/files/ebook.epub _site/2015/06/10/chandras-origin-fire-logic.html _site/2015/06/17/lilianas-origin-fourth-pact.html
